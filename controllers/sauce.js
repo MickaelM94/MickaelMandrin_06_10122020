a3cde789a36a5f68d4335d5258dc00559854da6a
MickaelM94 <58391020+MickaelM94@users.noreply.github.com>
2021-07-06 21:07:51 +0200

    Like & Dislike : ok

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -52,3 +52,72 @@ exports.deleteOneSauce = (req, res, next) => {
     })
     .catch(error => res.status(500).json({ error }))
 }
+
+exports.rateSauce = (req, res, next) => {
+    switch (req.body.like) {
+
+        // SI L'UTILISATEUR LIKE
+        case 1: Sauce.findByIdAndUpdate(req.params.id, { $inc: {likes: 1 }, $push: {usersLiked: req.body.userId}, _id: req.params.id })
+            .then(() => res.status(201).json({ message: "Liké !" }))
+            .catch(error => res.status(400).json({ error }));
+            break;
+    
+        // SI L'UTILISATEUR REVIENT SUR SA DECISION
+        case 0: Sauce.findOne({ _id: req.params.id })
+        .then(sauce => {
+
+            // POUR RETIRER LE LIKE
+            if (sauce.usersLiked.includes(req.body.userId)) {
+            Sauce.findByIdAndUpdate(req.params.id, { $inc: {likes: -1 }, $pull: {usersLiked: req.body.userId}, _id: req.params.id })
+            .then(() => res.status(201).json({ message: "Vous avez retiré votre like !" }))
+            .catch(error => res.status(400).json({ error }));
+            }
+
+            // POUR RETIRER LE DISLIKE
+            if (sauce.usersDisliked.includes(req.body.userId)) {
+                Sauce.findByIdAndUpdate(req.params.id, { $inc: {dislikes: -1 }, $pull: {usersDisliked: req.body.userId}, _id: req.params.id })
+                .then(() => res.status(201).json({ message: "Vous avez retiré votre dislike !" }))
+                .catch(error => res.status(400).json({ error }));
+            }
+        })
+        .catch(() => res.status(500).json({ message: "La sauce n'a pas été trouvée !"}))
+        break; 
+    
+        // SI L'UTILISATEUR DISLIKE
+        case -1: 
+            Sauce.findByIdAndUpdate(req.params.id, { $inc: {dislikes: 1 }, $push: {usersDisliked: req.body.userId}, _id: req.params.id })
+            .then(() => res.status(201).json({ message: "Disliké !" }))
+            .catch(error => res.status(400).json({ error }));
+            break;
+        }
+    }
+
+    
+  
+    //console.log(req.body.like);
+    // Sauce.findOne({ _id: req.params.id });
+  
+  
+    // switch (req.body.like) {
+    //     case -1: 
+    //         Sauce.updateOne({ $inc: {dislikes: 1 }, $push: {usersDisliked: req.body.userId}, _id: req.params.id })
+    //        .then(() => res.status(201).json({ message: "Disliké !" }))
+    //        .catch(error => res.status(400).json({ error }));
+    //       break;
+  
+    //     case 0: if (usersLiked.includes(req.body.userId)) {
+    //       Sauce.updateOne({ $inc: {likes: -1 }, $pull: {usersLiked: req.body.userId}, _id: req.params.id })
+    //       .then(() => res.status(201).json({ message: "Pas d'avis..." }))
+    //       .catch(error => res.status(400).json({ error }));
+    //     } else if(usersDisliked.includes(req.body.userId)) {
+    //       Sauce.updateOne({ $inc: {dislikes: -1 }, $pull: {usersDisliked: req.body.userId}, _id: req.params.id })
+    //       .then(() => res.status(201).json({ message: "Pas d'avis" }))
+    //       .catch(error => res.status(400).json({ error }));
+    //     }
+    //       break;
+  
+    //     case 1: Sauce.updateOne({ $inc: {likes: 1 }, $push: {usersLiked: req.body.userId}, _id: req.params.id })
+    //       .then(() => res.status(201).json({ message: "Liké !" }))
+    //       .catch(error => res.status(400).json({ error }));
+    //       break;
+    //   
--- routes/sauce.js
+++ routes/sauce.js
@@ -4,10 +4,14 @@ const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 const sauceCtrl = require('../controllers/sauce');
 
+// CRUD
 router.post('/', auth, multer, sauceCtrl.createSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteOneSauce);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.get('/', auth, sauceCtrl.getAllSauce);
+
+// LIKES & DISLIKES
+router.post('/:id/like', auth, sauceCtrl.rateSauce);
 
 module.exports = router;
